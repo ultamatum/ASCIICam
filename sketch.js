ed3dab660a3620ec56eb351f82f2152b51538c9b
Alexander Gray <alexander@ultamatum.dev>
2022-03-19 19:46:47 +0000

    Initial Commit, Camera is a bit jank need to find a way to auto scale

---
+++ sketch.js
@@ -0,0 +1,39 @@
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ '
+
+let video
+let asciiDiv
+
+function setup() {
+	noCanvas()
+	video = createCapture(VIDEO)
+	video.size(displayWidth / 7.5, displayHeight / 11)
+	video.hide()
+
+	asciiDiv = createDiv()
+}
+
+function draw() {
+	video.loadPixels()
+
+	let asciiImage = ''
+
+	for (let j = 0; j < video.height; j++) {
+		let row = ''
+		for (let i = 0; i < video.width; i++) {
+			const pixelIndex = (i + j * video.width) * 4
+			const r = video.pixels[pixelIndex + 0]
+			const g = video.pixels[pixelIndex + 1]
+			const b = video.pixels[pixelIndex + 2]
+			const avg = (r + g + b) / 3
+			const len = density.length
+			const charIndex = floor(map(avg, 0, 255, len, 0))
+
+			const c = density.charAt(charIndex)
+			if (c == ' ') asciiImage += '&nbsp;'
+			else asciiImage += c
+		}
+		asciiImage += '<br/>'
+	}
+
+	asciiDiv.html(asciiImage)
+}
